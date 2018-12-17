85a7e2f211b8880644a8e70f7c7fea53fa20cbef
RIsheek <risheek@pal-nat186-106-53.itap.purdue.edu>
2018-12-17 01:25:56 -0500

    intial commit 2.0

---
+++ src/main/java/com/nr/soggyapples/RisheekApplication.java
@@ -0,0 +1,14 @@
+package com.nr.soggyapples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RisheekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RisheekApplication.class, args);
+	}
+
+}
+
